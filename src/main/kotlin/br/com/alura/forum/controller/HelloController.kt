307a861a3b6decdc194402d547347e6e0275a8e6
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-09-09 08:56:52 -0300

    fix: Rename and organize hello controller

--- src/main/kotlin/br/com/alura/forum/controller/HelloController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/HelloController.kt
@@ -1,4 +1,4 @@
-package br.com.alura.forum
+package br.com.alura.forum.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/hello")
-class HelloControler {
+class HelloController {
 
     @GetMapping
     fun hello(): String {
