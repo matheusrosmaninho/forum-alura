087158d4eb17850cb77e31886b09cf56816dd68d
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-09-09 11:06:43 -0300

    feat: Create endpoint topico by id

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -7,9 +7,9 @@ import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class TopicoService {
-    fun listar(): List<Topico> {
-        val topico = Topico(
+class TopicoService(private var topicos: List<Topico>) {
+    init {
+        val topico1 = Topico(
             id = 1,
             titulo = "Example title",
             mensagem = "Test message",
@@ -24,6 +24,46 @@ class TopicoService {
                 email = "ana@example.com"
             )
         )
-        return Arrays.asList(topico, topico, topico)
+        val topico2 = Topico(
+            id = 2,
+            titulo = "Example title2",
+            mensagem = "Test message2",
+            curso = Curso(
+                id = 1,
+                nome = "Course 001",
+                categoria = "Example"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Ana",
+                email = "ana@example.com"
+            )
+        )
+        val topico3 = Topico(
+            id = 3,
+            titulo = "Example title3",
+            mensagem = "Test message3",
+            curso = Curso(
+                id = 1,
+                nome = "Course 001",
+                categoria = "Example"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Ana",
+                email = "ana@example.com"
+            )
+        )
+        topicos = listOf(topico1, topico2, topico3)
+    }
+
+    fun listar(): List<Topico> {
+        return topicos
+    }
+
+    fun buscarPorId(id: Long): Topico {
+        return topicos.stream().filter({
+            t -> t.id == id
+        }).findFirst().get()
     }
 }
